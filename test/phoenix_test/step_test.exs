0f3f8215ad158ba642bf7d426a4e4de69f4678ed
Fredrik Teschke <f@ftes.de>
2026-01-28 11:28:53 +0100

    Automatically verify step text in trace file

--- test/phoenix_test/step_test.exs
+++ test/phoenix_test/step_test.exs
@@ -2,6 +2,7 @@ defmodule PhoenixTest.StepTest do
   use PhoenixTest.Playwright.Case, async: true
 
   alias PhoenixTest.Playwright
+  alias PlaywrightEx.Tracing
 
   describe "step/3" do
     test "returns the conn unchanged", %{conn: conn_before} do
@@ -13,13 +14,12 @@ defmodule PhoenixTest.StepTest do
       assert conn_after == conn_before
     end
 
-    @tag :demo
-    @tag trace: :open
-    # The visibility of the labels must be confirmed manually in the trace viewer
     test "produces labels that can be seen in the trace viewer", %{conn: conn} do
+      {:ok, _} = Tracing.tracing_start(conn.tracing_id, timeout: timeout())
+      {:ok, _} = Tracing.tracing_start_chunk(conn.tracing_id, timeout: timeout())
+
       conn
       |> visit("/pw/live")
-      |> assert_has("h1", text: "Playwright")
       |> Playwright.step("Fill in form with test data", fn conn ->
         conn
         |> Playwright.step("Type into text input", fn conn ->
@@ -29,8 +29,17 @@ defmodule PhoenixTest.StepTest do
           assert_has(conn, "#changed-form-data", text: "text: Hello from custom step!")
         end)
       end)
-      |> click_link(nil, "Navigate", exact: true)
-      |> assert_has("h1", text: "Other")
+
+      {:ok, zip_file} = Tracing.tracing_stop_chunk(conn.tracing_id, timeout: timeout())
+      {:ok, _} = Tracing.tracing_stop(conn.tracing_id, timeout: timeout())
+
+      {:ok, handle} = :zip.zip_open(String.to_charlist(zip_file.absolute_path), [:memory])
+      {:ok, {_, trace_contents}} = :zip.zip_get(~c"trace.trace", handle)
+      :ok = :zip.zip_close(handle)
+
+      assert trace_contents =~ "Fill in form with test data"
+      assert trace_contents =~ "Type into text input"
+      assert trace_contents =~ "Verify form data changed"
     end
   end
 end
